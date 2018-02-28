837d1b7fab4369b13c74c97e213a1a39acb2d856
fengzhizi715 <fengzhizi715@126.com>
2018-02-28 11:11:00 +0800

    add VertxUtils

---
+++ sdk/src/main/java/com/cv4j/telegram/bot/utils/VertxUtils.java
@@ -0,0 +1,21 @@
+package com.cv4j.telegram.bot.utils;
+
+import io.vertx.core.Vertx;
+
+/**
+ * Created by tony on 2017/12/28.
+ */
+public class VertxUtils {
+
+    public static Vertx vertx = Vertx.vertx();
+
+    public static io.vertx.reactivex.core.Vertx reactivex_vertx = new io.vertx.reactivex.core.Vertx(vertx);
+
+    public static Vertx getVertx() {
+        return vertx;
+    }
+
+    public static void setVertx(Vertx vertx) {
+        VertxUtils.vertx = vertx;
+    }
+}
